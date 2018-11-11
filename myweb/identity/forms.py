bd885465535d6d39d6d98c971865874a32820080
wenchao-h <wenchao_h@outlook.com>
2018-11-11 22:58:34 +0800

    modify forms

--- myweb/identity/forms.py
+++ myweb/identity/forms.py
@@ -8,3 +8,11 @@ class loginForm(forms.Form):
     captcha = CaptchaField(label='验证码')
 
 
+class registerForm(forms.Form):
+    """ register form"""
+    nickname = forms.CharField(label='用户名', max_length=128, widget=forms.TextInput(attrs={'class':'from-control'}))
+    email = forms.EmailField(label='邮箱地址', widget=forms.EmailInput(attrs={'class':'form-control'}))
+    password1 = forms.CharField(label='密码', max_length=256, widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = form.CharField(label='确认密码', max_length=256, widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    captcha = CaptchaField(label='验证码')
+
